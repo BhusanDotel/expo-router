a22f59eb5d68dc842e3571bb8d1c9ccd16c1cb67
Bhusan Dotel <bhusandotel1@gmail.com>
2024-12-16 14:07:09 +0545

    First commit

---
+++ app/index.tsx
@@ -0,0 +1,63 @@
+import { router } from "expo-router";
+import { Text, TouchableOpacity, View } from "react-native";
+
+export default function HomeScreen() {
+  const pageNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "white",
+      }}
+    >
+      <Text
+        style={{
+          color: "black",
+        }}
+      >
+        Home Screen
+      </Text>
+
+      <View
+        style={{
+          marginTop: 20,
+          display: "flex",
+          flexWrap: "wrap",
+          flexDirection: "row",
+          gap: 10,
+        }}
+      >
+        {pageNumbers.map((pageNumber) => (
+          <TouchableOpacity
+            key={pageNumber}
+            onPress={() => {
+              router.push({
+                pathname: "pages/[pageNumber]",
+                params: {
+                  pageNumber: pageNumber.toString(),
+                  name: `Clicked page number ${pageNumber}`,
+                },
+              });
+            }}
+          >
+            <View
+              style={{
+                height: 20,
+                width: 20,
+                backgroundColor: "#6099fb",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <Text>{pageNumber}</Text>
+            </View>
+          </TouchableOpacity>
+        ))}
+      </View>
+    </View>
+  );
+}
---
+++ app/pages/[pageNumber].tsx
@@ -0,0 +1,51 @@
+import React from "react";
+import { View, Text, Pressable, StyleSheet } from "react-native";
+
+import { router, useLocalSearchParams } from "expo-router";
+
+const GoBackButton = () => {
+  return (
+    <Pressable style={styles.button} onPress={() => router.back()}>
+      <Text style={styles.buttonText}>Go Back</Text>
+    </Pressable>
+  );
+};
+
+const Pages = () => {
+  const local = useLocalSearchParams();
+  return (
+    <View style={styles.container}>
+      <GoBackButton />
+      <Text style={styles.content}>{local.pageNumber}</Text>
+      <Text>{local?.name}</Text>
+    </View>
+  );
+};
+
+// define your styles
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#f5f5f5",
+  },
+  button: {
+    backgroundColor: "#007BFF",
+    padding: 10,
+    borderRadius: 5,
+    marginBottom: 20,
+  },
+  buttonText: {
+    color: "#fff",
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+  content: {
+    fontSize: 18,
+    textAlign: "center",
+  },
+});
+
+//make this component available to the app
+export default Pages;
